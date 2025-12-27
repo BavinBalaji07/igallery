9c0c9a0821a9da200e7460c32d489f8c167f5fda
bavinbalaji13@gmail.com <bavinbalaji13@gmail.com>
2025-12-27 15:43:33 +0530

    Sucess

--- bavin/imgapp/static/text.js
+++ bavin/imgapp/static/text.js
@@ -1,26 +1,53 @@
-const modal = document.getElementById("imageModal");
 
-const fullImage = document.getElementById("fullImage");
 
-const captionText = document.getElementById("caption");
+var pics = [
+    {
+        image: "madara.jpg",
+        title: "Madara Uchiha",
+        info: "Ghost of the Uchihas"
+    },
+    {
+        image: "obito.jpeg",
+        title: "Obito Uchiha",
+        info: "Phantom of the Leaf Village"
+    },
+    {
+        image: "itachi uchiha.jpeg",
+        title: "Itachi Uchiha",
+        info: "The Uchiha Slayer."
+    },
+    {
+        image: "sas.jpeg",
+        title: "Sasuke Uchiha",
+        info: "The last of the Uchihas."
+    },
+];
 
-/**
- * Opens the modal and displays the full-size image.
- * @param {HTMLElement} imgElement - The clicked thumbnail image element.
- */
-function openModal(imgElement) {
-    modal.style.display = "block";
-    const fullSrc = imgElement.getAttribute("data-full");
-    fullImage.src = fullSrc;
-    captionText.innerHTML = imgElement.alt;
+var i = 0; 
+
+var photo     = document.getElementById("photo");
+var titleText = document.getElementById("title-text");
+var infoText  = document.getElementById("info-text");
+var countText = document.getElementById("count-text");
+
+function show() {
+    photo.src           = pics[i].image;
+    titleText.innerHTML = pics[i].title;
+    infoText.innerHTML  = pics[i].info;
+    countText.innerHTML = (i + 1) + " / " + pics.length;
 }
-
-function closeModal() {
-    modal.style.display = "none";
-
-window.onclick = function(event) {
-    if (event.target === modal) {
-        closeModal();
+function next() {
+    i++;
+    if (i >= pics.length) {
+        i = 0;
     }
+    show();
 }
+function prev() {
+    i--;
+    if (i < 0) {
+        i = pics.length - 1;
+    }
+    show();
 }
+show();
